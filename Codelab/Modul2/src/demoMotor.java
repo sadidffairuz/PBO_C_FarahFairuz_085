7f932468480601911412aa463815f08c46058b24
= <=>
2025-03-05 02:52:23 +0700

    ini nambahkan folder Modul 2

--- Codelab/Modul2/src/demoMotor.java
+++ Codelab/Modul2/src/demoMotor.java
@@ -1,5 +1,6 @@
 public class demoMotor {
     public static void main(String[] args) {
         System.out.println("Ini coba-coba saja, ya");
+        System.out.println("salah upload HEH");
     }
 }
